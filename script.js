0c5d15801d994ae8866d2524fc36e8be9e4639c1
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-16 19:16:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const timerHTML = document.getElementById("timer");
+setInterval(()=>{
+	let currTime = new Date();
+	timerHTML.innerHTML = currTime.toLocaleString();
+},1000);
